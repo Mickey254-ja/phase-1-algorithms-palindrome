81f1f913347c59bb5bdd69aab52c92aa6b73e5f8
Mickey254-ja <mickey.awino@student.moringaschool.com>
2022-12-13 04:38:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm hereif(word.length < 2) return true
+  const arrayOfLetters = word.split('');
+  const reverseLetters = arrayOfLetters.reverse()
+  const reverseString  = reverseLetters.join('')
 
-  let start = 0
-  let end = word.length - 1
-
-  while(start <= end){
-    if(word[start] != word[end]){
-      return false
-    }
-    start++
-    end--
-  }
+  if (word == reverseString){
 
     return true
 
+  } else {
+    return false
+  }
+  // Write your algorithm here
 }
 
 /* 
-  Add your pseudocode here
- -loop through the string
-  -check if last element != element at beginning
-  -if elements do not match return false
-  -else return true
-  -increment start
-  -decrement end 
+    Function isPalindrome has to take a string as a parements. 
+    If the string is palindrom, the function should return true. 
+    If not then the function should return false
 */
 
 /*
-  Add written explanation of your solution here
-  -A palindrome is a word that is pronunced the same oth ways. In my approach I have
-  created a function that allows a user to input a word. I start by checking if the length 
-  of the word is less than 2. A one letter word and an empty string are palindromes. I therefore 
-  return true if the word has a length of lesss than 2. I loop throug the string once if the length 
-  is greater or equal to 2. This happens in O(n) time and I do not require any extra space
-  to run the function. I return false when the last element does not match the first element.
+      The function takes the word and splits each letter into an array, then the array is reversed. The array is then joined again into a word. Then we compare if the orginal word is the same as the reversed word. 
+      If true then it returns true, or else it will return false
 */
 
 // You can run `node index.js` to view these console logs
